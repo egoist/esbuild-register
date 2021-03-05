76a9848f6ee44f19a0e1fb22c202905ca5e5038a
Anthony Fu <anthonyfu117@hotmail.com>
2021-03-05 23:31:18 +0800

    feat: use jsonc parser (#13)

--- src/options.ts
+++ src/options.ts
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import JoyCon from 'joycon'
-import strip from 'strip-json-comments'
+import { parse } from 'jsonc-parser'
 
 const joycon = new JoyCon()
 
@@ -8,7 +8,7 @@ joycon.addLoader({
   test: /\.json$/,
   loadSync: (file) => {
     const content = fs.readFileSync(file, 'utf8')
-    return JSON.parse(strip(content))
+    return parse(content)
   },
 })
 
